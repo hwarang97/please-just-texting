cb66928e92b28aaea079db95a955bb0b959cd48a
dolggul2 <dolggul2@gmail.com>
2024-08-18 11:18:52 +0900

    chore: remove adding template code.
    - this tmeplate do not use anymore.

--- app/main.py
+++ app/main.py
@@ -5,14 +5,12 @@ from app.routers import default
 from app.routers import users
 from app.settings import settings
 from fastapi import FastAPI
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 app.include_router(default.router)
 app.include_router(users.router)
 app.include_router(conversations.router)
 app.include_router(authentication.router)
-templates = Jinja2Templates(directory="app/templates")
 
 
 def run_app() -> None:
